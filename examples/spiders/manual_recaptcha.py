4e61af6d05eed4fecb26f0699c18075664e4f7e4
matthew <matvey.zvancov10@gmail.com>
2023-07-24 13:13:39 +0300

    Extended 1 example into 2. Checked grammar + beautified examples

--- examples/spiders/manual_recaptcha.py
+++ examples/spiders/manual_recaptcha.py
@@ -3,46 +3,42 @@ from twisted.python.failure import Failure
 
 from scrapypuppeteer import PuppeteerRequest
 from scrapypuppeteer.actions import GoTo, RecaptchaSolver, Click, Screenshot
-from scrapypuppeteer.response import PuppeteerResponse, PuppeteerJsonResponse
+from scrapypuppeteer.response import PuppeteerResponse, PuppeteerScreenshotResponse
+
+import base64
 
 
-class RecaptchaSpider(scrapy.Spider):
-    name = "recaptcha"
+class ManualRecaptchaSpider(scrapy.Spider):
+    name = "manual_recaptcha"
 
     start_urls = ["https://www.google.com/recaptcha/api2/demo"]
 
     def start_requests(self):
         for url in self.start_urls:
             action = GoTo(url=url)
-            yield PuppeteerRequest(action=action, callback=self.parse_html, errback=self.error, close_page=False)
+            yield PuppeteerRequest(action=action, callback=self.solve_recaptcha, errback=self.error, close_page=False)
 
     def solve_recaptcha(self, response: PuppeteerResponse, **kwargs):
-        # Activate this function and submit_recaptcha
-        # if you want to manually proceed recaptcha solving.
-        # Don't forget to turn off RecaptchaMiddleware
         action = RecaptchaSolver()
         yield response.follow(action=action, callback=self.submit_recaptcha, errback=self.error, close_page=False)
 
-    def submit_recaptcha(self, response: PuppeteerJsonResponse, **kwargs):
-        with open("metaData/meta.txt", 'w') as f:
-            print(response.data.get('recaptcha_data', None), file=f)
+    def submit_recaptcha(self, response, **kwargs):
         action = Click('#recaptcha-demo-submit')
         yield response.follow(action=action, callback=self.parse_html, errback=self.error, close_page=False)
 
-    def parse_html(self, response: PuppeteerResponse | PuppeteerJsonResponse, **kwargs):
-        with open(f"html/recaptcha_page.html", 'wb') as f:
+    def parse_html(self, response: PuppeteerResponse, **kwargs):
+        with open(f"recaptcha_page.html", 'wb') as f:
             f.write(response.body)
         action = Screenshot(options={
             'full_page': True,
         })
         yield response.follow(action,
-                              callback=self.save_image,
+                              callback=self.make_screenshot,
                               errback=self.error,
                               close_page=True)
 
-    def save_image(self, response, **kwargs):
+    def make_screenshot(self, response: PuppeteerScreenshotResponse, **kwargs):
         data = response.screenshot  # Note that data is string containing bytes, don't forget to decode them!
-        import base64
         with open("imageToSave.png", "wb") as fh:
             fh.write(base64.b64decode(data))
 
