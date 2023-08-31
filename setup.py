642d0fb3ceb90b11c13227511c33dab02a8e7b62
Max Varlamov <47754003+mxsnq@users.noreply.github.com>
2023-08-31 12:05:50 +0300

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='scrapy-puppeteer-client',
-    version='0.1.2',
+    version='0.1.3',
     description='A library to use Puppeteer-managed browser in Scrapy spiders',
     long_description=long_description,
     long_description_content_type="text/markdown",
