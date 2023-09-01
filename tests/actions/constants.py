5697c304a4a5abac8cebe8f87c36d459a4c008d5
matthew <matvey.zvancov10@gmail.com>
2023-09-01 12:40:04 +0300

    Added testing of actions

---
+++ tests/actions/constants.py
@@ -0,0 +1,23 @@
+from random import randint
+from itertools import combinations
+
+URLS = ("https://some_url.com", "https://invalid_url.ru", "https://one_more_url.net", "not_url/not_url")
+WAIT_UNTIL = ("load", "domcontentloaded", "networkidle0", "networkidle2")
+WAIT_OPTS = [None]
+SELECTORS = ("nothing", "tr.td::attr(something)", "#button", "")
+CLICK_OPTS = [None]
+
+
+def __gen_nav_opts():
+    options = [None]
+    for opt_num in range(1, 5):
+        for comb in combinations(WAIT_UNTIL, opt_num):
+            timeout = randint(0, 100) * 1000
+            options.append({
+                'timeout': timeout,
+                'waitUntil': list(comb),
+            })
+    return options
+
+
+NAV_OPTS = __gen_nav_opts()
---
+++ tests/actions/test_actions.py
@@ -0,0 +1,78 @@
+from pytest import mark
+from scrapypuppeteer.actions import GoTo, GoForward, GoBack, Click, Scroll
+from itertools import product
+from constants import URLS, NAV_OPTS, WAIT_OPTS, SELECTORS, CLICK_OPTS
+
+
+def _gen_goto():
+    for url, nav_opt, wait_opt in product(URLS, NAV_OPTS, WAIT_OPTS):
+        expected = {
+            'url': url,
+            'navigationOptions': nav_opt,
+            'waitOptions': wait_opt,
+        }
+        yield url, nav_opt, wait_opt, expected
+
+
+def _gen_back_forward():
+    for nav_opt, wait_opt in product(NAV_OPTS, WAIT_OPTS):
+        expected = {
+            'navigationOptions': nav_opt,
+            'waitOptions': wait_opt,
+        }
+        yield nav_opt, wait_opt, expected
+
+
+def _gen_click():
+    for selector, click_opt, nav_opt, wait_opt in product(SELECTORS, CLICK_OPTS, NAV_OPTS, WAIT_OPTS):
+        expected = {
+            'selector': selector,
+            'clickOptions': click_opt,
+            'waitOptions': wait_opt,
+            'navigationOptions': nav_opt,
+        }
+        yield selector, click_opt, nav_opt, wait_opt, expected
+
+
+def _gen_scroll():
+    for selector, wait_opt in product(SELECTORS, WAIT_OPTS):
+        expected = {
+            'selector': selector,
+            'waitOptions': wait_opt
+        }
+        yield selector, wait_opt, expected
+
+
+@mark.parametrize("url, navigation_options, wait_options, expected",
+                  _gen_goto())
+def test_goto(url, navigation_options, wait_options, expected):
+    action = GoTo(url, navigation_options, wait_options)
+    assert action.payload() == expected
+
+
+@mark.parametrize("navigation_options, wait_options, expected",
+                  _gen_back_forward())
+def test_go_forward(navigation_options, wait_options, expected):
+    action = GoForward(navigation_options, wait_options)
+    assert action.payload() == expected
+
+
+@mark.parametrize("navigation_options, wait_options, expected",
+                  _gen_back_forward())
+def test_go_forward(navigation_options, wait_options, expected):
+    action = GoBack(navigation_options, wait_options)
+    assert action.payload() == expected
+
+
+@mark.parametrize("selector, click_options, navigation_options, wait_options, expected",
+                  _gen_click())
+def test_click(selector, click_options, navigation_options, wait_options, expected):
+    action = Click(selector, click_options, wait_options, navigation_options)
+    assert action.payload() == expected
+
+
+@mark.parametrize("selector, wait_options, expected",
+                  _gen_scroll())
+def test_scroll(selector, wait_options, expected):
+    action = Scroll(selector, wait_options)
+    assert action.payload() == expected
