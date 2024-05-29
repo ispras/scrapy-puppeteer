1adcd5e73253d1bafc341e8c0808e761d64049c3
matthew <matvey.zvancov10@gmail.com>
2024-05-29 18:36:18 +0300

    Demo spider

--- scrapypuppeteer/middleware/__init__.py
+++ scrapypuppeteer/middleware/__init__.py
@@ -1,3 +1,3 @@
-from .service_middleware import PuppeteerServiceDownloaderMiddleware
-from .recaptcha_middleware import PuppeteerRecaptchaDownloaderMiddleware
-from .restore_middleware import PuppeteerContextRestoreDownloaderMiddleware
+from .service import PuppeteerServiceDownloaderMiddleware
+from .recaptcha import PuppeteerRecaptchaDownloaderMiddleware
+from .restore import PuppeteerContextRestoreDownloaderMiddleware
