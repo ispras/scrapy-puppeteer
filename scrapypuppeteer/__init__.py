0af6c3e82b8e9d741b244e6aab321b8c61daa747
Max Varlamov <varlamov@ispras.ru>
2024-08-22 17:27:34 +0300

    allow top-level import of actions

--- scrapypuppeteer/__init__.py
+++ scrapypuppeteer/__init__.py
@@ -1,3 +1,16 @@
+from .actions import (
+    PuppeteerServiceAction,
+    GoTo,
+    GoForward,
+    GoBack,
+    Click,
+    Scroll,
+    Screenshot,
+    Har,
+    FillForm,
+    RecaptchaSolver,
+    CustomJsAction,
+)
 from .request import PuppeteerRequest, CloseContextRequest
 from .response import (
     PuppeteerResponse,
